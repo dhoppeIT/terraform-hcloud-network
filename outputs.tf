ad1f0f6e4366782c453747b4047f0df2472958f2
Dennis Hoppe <github@dhoppe.it>
2022-01-14 12:28:24 +0100

    fix: Add additional outputs * id_network * id_subnet

--- outputs.tf
+++ outputs.tf
@@ -3,9 +3,19 @@ output "name" {
   value       = hcloud_network.default.name
 }
 
+output "id_network" {
+  description = "Unique ID of the network"
+  value       = hcloud_network.default.id
+}
+
 output "ip_range_network" {
   description = "IPv4 Prefix of the whole Network"
   value       = hcloud_network.default.ip_range
+}
+
+output "id_subnet" {
+  description = "ID of the Network subnet"
+  value       = hcloud_network_subnet.default.id
 }
 
 output "ip_range_subnet" {
